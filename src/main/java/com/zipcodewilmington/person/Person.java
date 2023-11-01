a76f1757255fde1b3a6f6b4eaf873a3c3bfb0bb2
hyunjialee <hyjialee@gmail.com>
2023-10-31 22:45:09 -0400

    finsihed

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,37 @@ public class Person {
     private int age;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
-
+// The name variable in the background is replaced by the new value that it is given
+// The classes name variable is set to whatever name value it is given.  -John Chen
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
-
+// The object already has a name and its just returning that name value
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
